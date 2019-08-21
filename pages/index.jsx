5a0ce4f85c0b6be4f79bfb67d0f4d68470449ce5
Przemek Sobstel <przemek@sobstel.org>
2019-08-21 12:15:38 +0200

    Setup tailwind css

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,37 +1,39 @@
-import React from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Nav from "../components/nav";
+// move to some Layout component
+import "../style.css";
 
 const Home = () => (
   <div>
     <Head>
-      <title>Home</title>
+      <title>Albicelestes - ¡Vamos Argentina! 🇦🇷</title>
     </Head>
 
     <Nav />
 
-    <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
+    <div className="hero">
+      <h1 className="title text-red-800">Welcome to Next.js!</h1>
+      <p className="description">
         To get started, edit <code>pages/index.js</code> and save to reload.
       </p>
 
-      <div className='row'>
-        <Link href='https://github.com/zeit/next.js#getting-started'>
-          <a className='card'>
+      <div className="row">
+        <Link href="https://github.com/zeit/next.js#getting-started">
+          <a className="card">
             <h3>Getting Started &rarr;</h3>
             <p>Learn more about Next.js on GitHub and in their examples</p>
           </a>
         </Link>
-        <Link href='https://github.com/zeit/next.js/tree/master/examples'>
-          <a className='card'>
+        <Link href="https://github.com/zeit/next.js/tree/master/examples">
+          <a className="card">
             <h3>Examples &rarr;</h3>
             <p>Find other example boilerplates on the Next.js GitHub</p>
           </a>
         </Link>
-        <Link href='https://github.com/zeit/next.js'>
-          <a className='card'>
+        <Link href="https://github.com/zeit/next.js">
+          <a className="card">
             <h3>Create Next App &rarr;</h3>
             <p>Was this tool helpful? Let us know how we can improve it!</p>
           </a>
@@ -86,6 +88,6 @@ const Home = () => (
       }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
