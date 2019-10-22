e49f0ccc40699e176435a23b30f34acc835055d5
Przemek Sobstel <przemek@sobstel.org>
2019-10-22 18:29:39 +0200

    test 6

--- db/hyena.js
+++ db/hyena.js
@@ -2,7 +2,7 @@ import low from "lowdb";
 import FileSync from "lowdb/adapters/FileSync";
 
 export const connect = () => {
-  const adapter = new FileSync(`${__dirname}/hyena.json`);
+  const adapter = new FileSync(`./db/hyena.json`);
   const db = low(adapter);
   return db;
 };
