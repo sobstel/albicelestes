056151b08c7de5191ff74e0cb248f3b87833dac6
Przemek Sobstel <przemek@sobstel.org>
2019-11-08 20:54:27 +0100

    add score progress to match goals

--- components/match/Goals.jsx
+++ components/match/Goals.jsx
@@ -1,9 +1,24 @@
-import { flatten, sortBy } from "lodash";
+import { flatten, forEach, last, map, sortBy, tap } from "lodash";
 import Section from "../layout/Section";
 import PlayerName from "../PlayerName";
 
+function indexGoals(matchGoals) {
+  return forEach(matchGoals, (goals, teamIndex) => {
+    forEach(goals, goal => (goal.teamIndex = teamIndex));
+  });
+}
+
+function addScores(goals) {
+  const score = [0, 0];
+  return map(goals, goal => {
+    score[goal.teamIndex] += 1;
+    goal.score = [...score];
+    return goal;
+  });
+}
+
 export default ({ match }) => {
-  const goals = sortBy(flatten(match.goals), "min");
+  const goals = addScores(sortBy(flatten(indexGoals(match.goals)), "min"));
 
   if (goals.length === 0) {
     return null;
@@ -13,6 +28,7 @@ export default ({ match }) => {
     <Section title="Goals">
       {goals.map((goal, index) => (
         <p key={index}>
+          {goal.score && `${goal.score.join(":")} `}
           <PlayerName name={goal.name} match={match} id={goal.id} />{" "}
           {goal.min && `${goal.min}'`}
           {goal.type !== "G" && ` [${goal.type}] `}
