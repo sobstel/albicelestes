09f91659ba3928bf585abac8a146902415ab5b4b
Przemek Sobstel <przemek@sobstel.org>
2019-11-08 20:56:01 +0100

    :lipstick::dancer:

--- components/match/Goals.jsx
+++ components/match/Goals.jsx
@@ -28,7 +28,7 @@ export default ({ match }) => {
     <Section title="Goals">
       {goals.map((goal, index) => (
         <p key={index}>
-          {goal.score && `${goal.score.join(":")} `}
+          {goal.score.join(":")}{" "}
           <PlayerName name={goal.name} match={match} id={goal.id} />{" "}
           {goal.min && `${goal.min}'`}
           {goal.type !== "G" && ` [${goal.type}] `}
