24cb78e0ccfdaa254c0bbd63b365bf0f149755c6
Przemek Sobstel <przemek@sobstel.org>
2019-11-08 11:10:08 +0100

    simplify deduplicateNames()

--- components/PlayerName.jsx
+++ components/PlayerName.jsx
@@ -1,9 +1,11 @@
 import {
+  countBy,
   filter,
   flatten,
   forEach,
   fromPairs,
-  mapValues,
+  keys,
+  map,
   memoize,
   pickBy,
   reduce,
@@ -13,50 +15,50 @@ import {
 import slugify from "slugify";
 import Link from "next/link";
 
-function skipFirstName(name) {
+function cutFirstName(name) {
   return name
     .split(" ")
     .slice(1)
     .join(" ");
 }
 
-function deduplicateNames(shortNames) {
-  const uniqueNames = shortNames;
+function deduplicateNames(indexedNames) {
+  const indexedNamesUniq = indexedNames;
 
-  let duplicates;
-  let i = 1;
-  let duplicateFirstNameMaxLength;
+  let duplicateNames;
+  let i = 0;
 
   do {
-    duplicates = pickBy(
-      uniqueNames,
-      value =>
-        filter(uniqueNames, otherValue => value === otherValue).length > 1
-    );
-    forEach(duplicates, (shortName, fullName) => {
-      uniqueNames[fullName] = `${fullName.slice(0, i)}. ${skipFirstName(
-        fullName
-      )}`;
-    });
     i += 1;
 
-    duplicateFirstNameMaxLength = reduce(
-      duplicates,
-      (length, _, fullName) => {
-        return Math.max(skipFirstName(fullName).length, length);
-      },
-      0
+    duplicateNames = keys(
+      pickBy(countBy(indexedNamesUniq), count => count > 1)
     );
-  } while (size(duplicates) > 0 || i < duplicateFirstNameMaxLength - 1);
 
-  return uniqueNames;
+    forEach(duplicateNames, name => {
+      const duplicateIndexes = keys(
+        pickBy(indexedNamesUniq, value => value === name)
+      );
+
+      forEach(duplicateIndexes, fullName => {
+        const firstNameLength = fullName.indexOf(" ");
+        if (firstNameLength < i) return;
+
+        const shortenedFirstName = fullName.slice(0, i);
+        const lastName = cutFirstName(fullName);
+
+        indexedNamesUniq[fullName] = `${shortenedFirstName}. ${lastName}`;
+      });
+    });
+  } while (size(duplicateNames) > 0 && i < 8);
+
+  return indexedNamesUniq;
 }
 
 const shortenFirstNames = memoize(function(match) {
   const names = flatten(match.lineups).map(appearance => appearance.name);
-  const indexedNames = fromPairs(zip(names, names));
-  const shortNames = mapValues(indexedNames, skipFirstName);
-  return deduplicateNames(shortNames);
+  const indexedNames = fromPairs(zip(names, map(names, cutFirstName)));
+  return deduplicateNames(indexedNames);
 });
 
 function PlayerName({ name, match, id = null }) {
