75bca24ece5ad58e1ff0f900fc52422cb84b360f
Przemek Sobstel <przemek@sobstel.org>
2019-11-11 10:20:18 +0100

    ruby test (cowsay)

---
+++ pages/api/test/Gemfile
@@ -0,0 +1,3 @@
+source "https://rubygems.org"
+
+gem "cowsay", "~> 0.3.0"
--- pages/api/test/index.rb
+++ pages/api/test/index.rb
@@ -1,5 +1,7 @@
+require 'cowsay'
+
 Handler = Proc.new do |req, res|
-  res.status = 200
-  res['Content-Type'] = 'text/plain'
-  res.body = 'ruby test'
+    res.status = 200
+    res['Content-Type'] = 'text/plain'
+    res.body = Cowsay.say('hello world', 'cow')
 end
