1761134ed74cb1f9c9f8367eed80b4827bdd7490
Przemek Sobstel <przemek@sobstel.org>
2019-12-01 17:24:48 +0100

    home: major matches

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,19 @@ import Layout from "components/Layout";
 import Fixtures from "components/Fixtures";
 
 type Props = {
-  matches: PartialMatch[];
+  recentMatches: PartialMatch[];
+  majorMatches: PartialMatch[];
 }
 
-const IndexPage = ({ matches }: Props) => (
+const IndexPage = ({ recentMatches, majorMatches }: Props) => (
   <Layout title="Ultimate La Selección Database">
-    <Fixtures title="Recent matches" matches={matches} />
+    <Fixtures title="Recent matches" matches={recentMatches} />
+    <Fixtures title="Major matches" matches={majorMatches} />
   </Layout>
 );
 
 IndexPage.getInitialProps = async ({ res }: any) => {
-  const result = await internalAPI(`matches/recent`);
+  const result = await internalAPI(`home`);
 
   if (res) {
     res.setHeader(
@@ -22,8 +24,8 @@ IndexPage.getInitialProps = async ({ res }: any) => {
     );
   }
 
-  const { matches } = result;
-  return { matches };
+  const { recentMatches, majorMatches } = result;
+  return { recentMatches, majorMatches };
 };
 
 export default IndexPage;
