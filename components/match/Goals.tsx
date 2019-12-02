ee27257230f043312d92dc0ffd00e5859f94f990
Przemek Sobstel <przemek@sobstel.org>
2019-12-02 10:47:08 +0100

    stricter typing for indexGoals and addScores funcs

--- components/match/Goals.tsx
+++ components/match/Goals.tsx
@@ -6,17 +6,18 @@ type Props = { match: Match };
 type IndexedGoal = Goal & { teamIndex: number };
 type GoalWithScore = Goal & { score: Score };
 
-function indexGoals(matchGoals: [Goal[], Goal[]]): IndexedGoal[][] {
-  return matchGoals.map((goals, teamIndex) =>
-    goals.map(goal => ({ ...goal, teamIndex }))
-  );
+function indexGoals(matchGoals: [Goal[], Goal[]]): [IndexedGoal[], IndexedGoal[]] {
+  return [
+    matchGoals[0].map(goal => ({ ...goal, teamIndex: 0 })),
+    matchGoals[1].map(goal => ({ ...goal, teamIndex: 1 })),
+  ];
 }
 
 function addScores(goals: IndexedGoal[]): GoalWithScore[] {
   const currentScore = [0, 0];
   return goals.map(goal => {
     currentScore[goal.teamIndex] += 1;
-    return { ...goal, score: [...currentScore] as [number, number] };
+    return { ...goal, score: [currentScore[0], currentScore[1]] };
   });
 }
 
