c110b27e0aeb37b0ec528fdd7c2b6344f1bf2586
Przemek Sobstel <przemek@sobstel.org>
2020-01-02 13:16:37 +0100

    Don't reverse matches at home page (for consistency)

--- pages/api/home.js
+++ pages/api/home.js
@@ -1,4 +1,13 @@
-import { filter, flow, get, includes, map, pick, reverse, takeRight } from "lodash";
+import {
+  filter,
+  flow,
+  get,
+  includes,
+  map,
+  pick,
+  reverse,
+  takeRight
+} from "lodash";
 import data from "db/data";
 
 function pickMatch(match) {
@@ -15,17 +24,18 @@ function pickMatch(match) {
 
 export default async function handle(_req, res) {
   const recentMatches = flow(
-    (data) => get(data, "matches"),
-    (matches) => takeRight(matches, 7),
-    (matches) => map(matches, pickMatch),
-    reverse
+    data => get(data, "matches"),
+    matches => takeRight(matches, 7),
+    matches => map(matches, pickMatch)
   )(data);
 
   const majorMatches = flow(
-    (data) => get(data, "matches"),
-    (matches) => filter(matches, match => includes(["lk9o5", "xrm53", "k3ype", "d5lq7", "0ldxk"], match.id)),
-    (matches) => map(matches, pickMatch),
-    reverse
+    data => get(data, "matches"),
+    matches =>
+      filter(matches, match =>
+        includes(["lk9o5", "xrm53", "k3ype", "d5lq7", "0ldxk"], match.id)
+      ),
+    matches => map(matches, pickMatch)
   )(data);
 
   res.json({ recentMatches, majorMatches });
