4a68fd6f24d200a20fed61f8d6800b02ec70448f
Przemek Sobstel <przemek@sobstel.org>
2020-01-11 20:48:08 +0100

    extract players nav to separate component

---
+++ components/players/Nav.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+function Nav({ catalog }: { catalog?: string }) {
+  return (
+    <ul className="mb-4">
+      {"abcdefghijklmnopqrstuvwxyz".split("").map(localCatalog => {
+        const textColor =
+          catalog !== localCatalog
+            ? "text-blue-600 hover:text-blue-400"
+            : "text-black";
+        return (
+          <li className="mr-4 inline-flex">
+            <Link href="/players/[catalog]" as={`/players/${localCatalog}`}>
+              <a className={`${textColor} font-semibold uppercase`}>
+                {localCatalog}
+              </a>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default Nav;
--- pages/players/[catalog]/index.tsx
+++ pages/players/[catalog]/index.tsx
@@ -1,11 +1,13 @@
 import Layout from "components/Layout";
+import Nav from "components/players/Nav";
 
 type Props = { catalog: string };
 
 const PlayersPage = ({ catalog }: Props) => {
   return (
     <Layout title={`${catalog} | Argentina Players`}>
-      <h2 className="mb-4 font-semibold uppercase">Players ({catalog})</h2>
+      <Nav catalog={catalog} />
+      <h2 className="mb-4 font-semibold uppercase">Players</h2>
       <p>TODO</p>
     </Layout>
   );
