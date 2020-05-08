54e66f70114553bcd31eafdb144d12d28de30a59
Przemek Sobstel <przemek@sobstel.org>
2020-05-08 19:53:38 +0200

    Update verification text

--- components/Page/Match/VerifiedNote.tsx
+++ components/Page/Match/VerifiedNote.tsx
@@ -9,7 +9,7 @@ export default function VerifiedNote({ match: { verified } }: Props) {
   if (!verified) {
     return (
       <p className="pt-2 text-xs">
-        Data not verified for this game yet.
+        Data might not be accurate for this game yet. It needs to be verified.
         <br />
         Want to help? Drop me a line at przemek&#64;sobstel&#46;org.
       </p>
