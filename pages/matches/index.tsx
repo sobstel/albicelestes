7e71e01388db14f2a2383d73aa45bbfc2641b5b9
Przemek Sobstel <przemek@sobstel.org>
2020-05-12 00:34:14 +0200

    Fix matches page

--- pages/matches/index.tsx
+++ pages/matches/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { MAX_YEAR } from "config";
 import Page, { Props } from "components/Page/Matches";
 
 export default function PageContainer(props: Props) {
-  return <Page {...props} />;
+  return <Page {...props} year={MAX_YEAR.toString()} />;
 }
