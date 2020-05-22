9ead40d2f1655dce44829dd7e3d8cfa6221f6fd2
Przemek Sobstel <przemek@sobstel.org>
2020-05-22 19:17:13 +0200

    more concise matches.json

--- db/index.ts
+++ db/index.ts
@@ -3,8 +3,8 @@ import { matchSlug, matchYear, playerCatalog, playerSlug } from '../helpers';
 import { Match, MatchInfo, MatchItem, PlayerInfo, Bibliography } from "types";
 
 // note: "import" is not used to prevent tsc from checking files that are too big
-export const fetchMatches = R.once((): Match[] => (require('./matches.json')));
 export const fetchInflections = R.once((): Record<string, string> => (require('./inflections.json')));
+export const fetchMatches = R.once((): Match[] => (require('./matches.json')));
 
 export function fetchMatchInfo(match: MatchItem): MatchInfo {
   try {
