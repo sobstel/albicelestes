3523bfa7625cf43730ab4b889fdf907245fe8475
Przemek Sobstel <przemek@sobstel.org>
2020-05-23 16:30:51 +0200

    try next.config.js for redirects

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  experimental: {
+    async redirects() {
+      return [
+        {
+          source: "/matches/:year/:slug/:id",
+          destination: "/matches/:year/:slug",
+        },
+      ];
+    },
+  },
+};
