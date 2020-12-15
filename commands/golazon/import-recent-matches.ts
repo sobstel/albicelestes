c61bfbc3b5402fb7904e6344af1fc1e74bfffc87
Przemek Sobstel <przemek@sobstel.org>
2020-12-15 20:47:56 +0100

    import-recent-matches partial mapping

--- commands/golazon/import-recent-matches.ts
+++ commands/golazon/import-recent-matches.ts
@@ -2,13 +2,44 @@ import * as R from "remeda";
 import got from "got";
 import { fetchMatches } from "db";
 import { spinner } from "utility/command";
+import util from "util";
+
 // import jsonStringify from "utility/jsonStringify";
 
 const TEAM_URL = "https://golazon.com/api/teams/03l";
 type TeamResponse = { recentFixtures: { match_id: string; date: string }[] };
 
 const MATCH_URL = "https://golazon.com/api/matches/ID";
-type MatchResponse = Record<string, unknown>;
+
+// TODO: move to hyena
+type Person = { person_id: string; name: string };
+type Appearance = Person & { in?: string; out?: string };
+type Goal = Person & { code: string; score: [number, number]; min: string };
+type Card = Person & { code: string; min: string };
+type MatchResponse = {
+  match_id: string;
+  date: string;
+  time: string;
+  home_id: string;
+  home_name: string;
+  away_id: string;
+  away_name: string;
+  ended?: boolean;
+  ft: [number, number];
+  ht: [number, number];
+  goals: Goal[];
+  penalty_shootout: unknown;
+  cards: Card[];
+  home_players: Appearance[];
+  home_coach: Person;
+  away_players: Appearance[];
+  away_coach: Person;
+  competition_id: string;
+  competition_name: string;
+  area_name: string;
+  round_name: string;
+  teamtype: string | null;
+};
 
 export default async () => {
   spinner.next("Fetch recent matches from Golazon API");
@@ -36,23 +67,49 @@ export default async () => {
 
   apiResponses.forEach((response) => {
     const match = JSON.parse(response.body) as MatchResponse;
-    match;
-    //   /* eslint-disable @typescript-eslint/camelcase */
-    //   console.log({
-    //     slug: "", // TODO: helper to generate slug
-    //     match_id: match["match_id"],
-    //     date: match["date"],
-    //     // competition: match["competition"]?.name,
-    //     teams: [match["home_name"], match["away_name"]],
-    //   });
-    //   /* eslint-enable */
-    //   // TODO: match popular names (if no other similar)
-    //   // TODO: (?) have index of names by person_id
-    //   // TODO: use inquirer
-    //   // dbMatches = dbMatches.concat(newMatch);
-    //   // TODO: save to db
-    //   // dbMatches
-    //   // jsonStringify();
+    /* eslint-disable @typescript-eslint/camelcase */
+    console.log(
+      util.inspect(
+        {
+          slug: "", // TODO: helper to generate slug
+          match_id: match["match_id"],
+          date: match.date,
+          competition: match["competition_name"],
+          // TODO: venue
+          round: match["round_name"], // TODO: add to albicelestes types
+          teams: [match["home_name"], match["away_name"]],
+          score: match.ft,
+          // TODO: pen
+          // TODO: result (W/L/D)
+          // TODO: goals: match.goals (partition and save only selected data)
+          // TODO: cards: match.cards, (partition and ssave only selected data)
+          coaches: [
+            { name: match["home_coach"].name },
+            { name: match["away_coach"].name },
+          ],
+          lineups: [match["home_players"], match["away_players"]].map((apps) =>
+            apps.map((app) => ({
+              name: app.name,
+              ...(app.in && { in: app.in }),
+              ...(app.out && { out: app.out }),
+            }))
+          ),
+          // TODO: sources
+          // TODO: suspended?
+          // TODO: penaltyShootout
+        },
+        { depth: 4 }
+      )
+    );
+    /* eslint-enable */
+
+    // TODO: match popular names (if no other similar)
+    // TODO: (?) have index of names by person_id
+    // TODO: use inquirer
+    // dbMatches = dbMatches.concat(newMatch);
+    // TODO: save to db
+    // dbMatches
+    // jsonStringify();
   });
 
   spinner.done();
