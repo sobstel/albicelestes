c98a960b90435049a178e6657dd5278c775cacdb
Przemek Sobstel <przemek@sobstel.org>
2021-01-17 18:26:38 +0100

    fix deployment

--- pages/matches/index.tsx
+++ pages/matches/index.tsx
@@ -1,7 +1,1 @@
-import React from "react";
-import { MAX_YEAR } from "config";
-import Page, { Props } from "components/Page/Matches";
-
-export default function PageContainer(props: Props) {
-  return <Page {...props} year={MAX_YEAR.toString()} />;
-}
+export { default, getStaticProps } from "./[year]";
