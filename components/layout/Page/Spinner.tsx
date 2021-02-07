4495d64e4f556c640c15dfd72d01f0e260ea8f62
Przemek Sobstel <przemek@sobstel.org>
2021-02-07 12:32:44 +0100

    Use custom spinner instead of nprogress (#40)
    * Use custom spinner instead of default one
    
    * :lipstick:
    
    * Lower project codecov requirement to 78%
    
    * require_ci_to_pass=no
    
    * move custom styles into container

---
+++ components/layout/Page/Spinner.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import Router from "next/router";
+
+export default function Spinner() {
+  const [isVisible, setVisible] = useState(false);
+
+  useEffect(() => {
+    Router.events.on("routeChangeStart", () => setVisible(true));
+    Router.events.on("routeChangeComplete", () => setVisible(false));
+    Router.events.on("routeChangeError", () => setVisible(false));
+  }, []);
+
+  if (!isVisible) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="_spinner" role="spinner">
+        <div className="_spinner__icon"></div>
+      </div>
+      <style jsx>{`
+        ._spinner {
+          display: block;
+          position: fixed;
+          z-index: 666;
+          top: 1rem;
+          right: 1rem;
+        }
+
+        ._spinner__icon {
+          width: 1rem;
+          height: 1rem;
+
+          border: solid 2px transparent;
+          border-top-color: #000;
+          border-left-color: #000;
+          border-radius: 50%;
+
+          -webkit-animation: pinner 400ms linear infinite;
+          animation: spinner 400ms linear infinite;
+        }
+
+        @-webkit-keyframes spinner {
+          0% {
+            -webkit-transform: rotate(0deg);
+          }
+          100% {
+            -webkit-transform: rotate(360deg);
+          }
+        }
+        @keyframes spinner {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+    </>
+  );
+}
