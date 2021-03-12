eb400b4f3a49f60a31a537b5a43174a9e4451333
Przemek Sobstel <przemek@sobstel.org>
2021-03-12 16:14:11 +0100

    Import images from cloudinary (#46)
    * cli config
    
    * import images to repo from cloudinary
    
    * lazy-load images and videos (iframes)
    
    * bump node version in builds
    
    * :lipstick::dancer:
    
    * update github actions versions

--- components/Match/Info.tsx
+++ components/Match/Info.tsx
@@ -25,29 +25,30 @@ export default function Info({ match }: Props) {
                   frameBorder="0"
                   allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
+                  loading="lazy"
                 />
               </div>
-              <style jsx>{`
-                ._video-container {
-                  overflow: hidden;
-                  position: relative;
-                  width: 100%;
-                }
-                ._video-container::after {
-                  padding-top: 56.25%;
-                  display: block;
-                  content: "";
-                }
-                ._video-container iframe {
-                  position: absolute;
-                  top: 0;
-                  left: 0;
-                  width: 100%;
-                  height: 100%;
-                }
-              `}</style>
             </div>
           ))}
+          <style jsx>{`
+            ._video-container {
+              overflow: hidden;
+              position: relative;
+              width: 100%;
+            }
+            ._video-container::after {
+              padding-top: 56.25%;
+              display: block;
+              content: "";
+            }
+            ._video-container iframe {
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+            }
+          `}</style>
         </Block>
       )}
       {images && (
@@ -56,13 +57,8 @@ export default function Info({ match }: Props) {
           {images.map((image) => {
             return (
               <p key={image.url} className="mb-2">
-                <a
-                  href={image.url}
-                  className="text-link hover:text-link-hover"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={image.url} className="w-full" />
+                <a href={image.url} target="_blank" rel="noopener noreferrer">
+                  <img src={image.url} loading="lazy" className="w-full" />
                 </a>
               </p>
             );
