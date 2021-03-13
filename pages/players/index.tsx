4481b90b13c799834969d59816d06b0b7d2e82b9
Przemek Sobstel <przemek@sobstel.org>
2021-03-13 13:10:51 +0100

    Suggest similar players if player slug not found (#47)
    * add static 404 page
    
    * suggest players on wrong slug

--- pages/players/index.tsx
+++ pages/players/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import * as R from "remeda";
 import { fetchMatches } from "data";
 import { collectPlayers, sortByMatchesPlayed } from "helpers";
-import { getPlayerName, getPlayerCatalog, getPlayerSlug } from "helpers";
-import { Page, Block, LinkAnchor } from "components/layout";
-import PlayerCatalogNav from "components/PlayerCatalogNav";
 import { PlayerItem } from "types";
+import { Page } from "components/layout";
+import PlayerCatalogNav from "components/PlayerCatalogNav";
+import PlayerList from "components/PlayerList";
 
 export async function getStaticProps() {
   const players = R.pipe(
@@ -32,32 +32,7 @@ export default function PlayerIndexPage({ catalog, players }: Props) {
   return (
     <Page title={titleParts}>
       <PlayerCatalogNav catalog={catalog} />
-
-      {players && players.length > 0 && (
-        <Block>
-          {players.map(({ name, mp }) => {
-            const _playerName = getPlayerName(name);
-            const slug = getPlayerSlug(name);
-            const catalog = getPlayerCatalog(name);
-
-            return (
-              <p key={slug}>
-                <LinkAnchor
-                  href={`/players/${catalog}/${slug}`}
-                  title={name}
-                  important={!!mp && mp >= 20}
-                >
-                  {_playerName.lastName},{" "}
-                  {[_playerName.firstName, _playerName.middleName]
-                    .join(" ")
-                    .trim()}
-                </LinkAnchor>{" "}
-                ({mp})
-              </p>
-            );
-          })}
-        </Block>
-      )}
+      <PlayerList players={players} />
     </Page>
   );
 }
