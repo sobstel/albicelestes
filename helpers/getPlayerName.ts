dc8c71eba4e196d1f89e2fc94067156812c24719
Przemek Sobstel <przemek@sobstel.org>
2021-06-17 22:08:12 +0200

    prettify getPlayerName

--- helpers/getPlayerName.ts
+++ helpers/getPlayerName.ts
@@ -2,23 +2,21 @@ import * as R from "remeda";
 import memoize from "lodash.memoize";
 import { PlayerName } from "types";
 
-export const getPlayerName = memoize(
-  (name: string): PlayerName => {
-    const nameParts = name.split(" ");
+export const getPlayerName = memoize((name: string): PlayerName => {
+  const nameParts = name.split(" ");
 
-    let lastName = nameParts.pop() || "";
-    const firstName = nameParts.shift() || "";
+  let lastName = nameParts.pop() || "";
+  const firstName = nameParts.shift() || "";
 
-    const lastNamePart = R.last(nameParts) || "";
-    if (lastNamePart.length >= 2 && lastNamePart.length <= 3) {
-      lastName = [lastNamePart, lastName].join(" ");
-      nameParts.pop();
-    }
-
-    const middleName = nameParts.join(" ") || "";
-
-    return { firstName, middleName, lastName };
+  const lastNamePart = R.last(nameParts) || "";
+  if (lastNamePart.length >= 2 && lastNamePart.length <= 3) {
+    lastName = [lastNamePart, lastName].join(" ");
+    nameParts.pop();
   }
-);
+
+  const middleName = nameParts.join(" ") || "";
+
+  return { firstName, middleName, lastName };
+});
 
 export default getPlayerName;
