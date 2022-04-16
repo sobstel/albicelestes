cc4ec67cf0a62ed57674480dbe35530dcd8cb506
Przemek Sobstel <przemek@sobstel.dev>
2022-04-16 14:20:49 +0200

    use selects instead of prev/next

--- app/components/YearlyNav.tsx
+++ app/components/YearlyNav.tsx
@@ -1,5 +1,11 @@
 import debounce from "lodash.debounce";
-import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import React, {
+  ChangeEventHandler,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { useNavigate } from "react-router-dom";
 import * as R from "remeda";
 
@@ -10,9 +16,33 @@ import { MAX_YEAR, MIN_YEAR } from "~/config";
 const isBrowser = typeof window !== "undefined";
 const useIsomorphicLayoutEffect = isBrowser ? useLayoutEffect : useEffect;
 
+const Select = ({
+  values,
+  selectedValue,
+  handleChange,
+}: {
+  values: Array<string>;
+  selectedValue?: string;
+  handleChange: React.ChangeEventHandler<HTMLSelectElement>;
+}) => {
+  return (
+    <select
+      onChange={handleChange}
+      className="mx-1 outline-none"
+      disabled={values.length === 0}
+    >
+      {values.map((value) => (
+        <option key={value} selected={value === selectedValue}>
+          {value}
+        </option>
+      ))}
+    </select>
+  );
+};
+
 function DefaultNav({ activeYear }: { activeYear?: string }) {
+  const navigate = useNavigate();
   const currentYear = activeYear ?? String(MAX_YEAR);
-  const anyYearActive = Boolean(activeYear);
 
   const [yearRange, setYearRange] = useState<[string, string]>([
     currentYear,
@@ -20,44 +50,53 @@ function DefaultNav({ activeYear }: { activeYear?: string }) {
   ]);
   const currentYearInt = parseInt(currentYear, 10);
 
-  const prevNextShift =
-    parseInt(yearRange[1], 10) - parseInt(yearRange[0], 10) + 1;
-
-  const prevYear =
-    anyYearActive &&
-    currentYearInt > MIN_YEAR &&
-    Math.max(currentYearInt - prevNextShift, MIN_YEAR);
-  const nextYear =
-    anyYearActive &&
-    currentYearInt < MAX_YEAR &&
-    Math.min(currentYearInt + prevNextShift, MAX_YEAR);
   const containerRef = useRef<HTMLElement>(null);
   const itemRef = useRef<HTMLElement>(null);
+  const lowerSelectRef = useRef<HTMLSelectElement>(null);
+  const upperSelectRef = useRef<HTMLSelectElement>(null);
 
   useIsomorphicLayoutEffect(() => {
     const handleChange = () => {
-      if (containerRef?.current && itemRef?.current) {
+      if (
+        containerRef?.current &&
+        itemRef?.current &&
+        lowerSelectRef?.current &&
+        upperSelectRef?.current
+      ) {
         const containerWidth = Math.floor(
           containerRef.current.getBoundingClientRect().width
         );
-        const itemWidth = Math.ceil(
-          itemRef.current.getBoundingClientRect().width
-        );
-        const itemsCount = Math.floor(containerWidth / itemWidth);
+        const itemWidth =
+          Math.ceil(itemRef.current.getBoundingClientRect().width) + 8; // margin
+        const selectsWidth =
+          Math.ceil(lowerSelectRef.current.getBoundingClientRect().width) +
+          Math.ceil(upperSelectRef.current.getBoundingClientRect().width);
 
-        let lowerLimit =
-          parseInt(currentYear, 10) - Math.ceil(itemsCount) / 2 + 2; // plus PREV & CURYEAR
-        let upperLimit =
-          parseInt(currentYear, 10) + Math.floor(itemsCount) / 2 - 1; // minus NEXT
+        const itemsCount = Math.floor(
+          (containerWidth - selectsWidth) / itemWidth
+        );
+
+        let lowerLimit = parseInt(currentYear, 10) - Math.ceil(itemsCount) / 2;
+        let upperLimit = parseInt(currentYear, 10) + Math.floor(itemsCount) / 2;
 
         if (lowerLimit < MIN_YEAR) {
           lowerLimit = MIN_YEAR;
-          upperLimit = MIN_YEAR + itemsCount - 3; // minus PREV & CURYEAR & NEXT
+          upperLimit = MIN_YEAR + itemsCount - 1; // - CURYEAR
         }
         if (upperLimit > MAX_YEAR) {
-          lowerLimit = MAX_YEAR - itemsCount + 3; // minus PREV & CURYEAR & NEXT
+          lowerLimit = MAX_YEAR - itemsCount + 1; // + CURYEAR
           upperLimit = MAX_YEAR;
         }
+
+        console.log({
+          containerWidth,
+          itemWidth,
+          selectsWidth,
+          itemsCount,
+          lowerLimit,
+          upperLimit,
+        });
+
         setYearRange([String(lowerLimit), String(upperLimit)]);
       }
     };
@@ -66,44 +105,64 @@ function DefaultNav({ activeYear }: { activeYear?: string }) {
     window.addEventListener("resize", handleResize);
     handleChange();
     return () => window.removeEventListener("resize", handleResize);
-  }, [currentYear, containerRef, itemRef]);
+  }, [currentYear, containerRef, itemRef, lowerSelectRef, upperSelectRef]);
 
-  const years = R.pipe(
-    R.range(parseInt(yearRange[0], 10), parseInt(yearRange[1], 10) + 1),
-    R.map(String)
-  );
+  const handleYearChange: ChangeEventHandler<HTMLSelectElement> = ({
+    target: { value },
+  }) => {
+    navigate(`/${value}`);
+  };
 
   return (
-    <nav className="grow overflow-hidden" ref={containerRef}>
+    <nav className="grow overflow-hidden flex" ref={containerRef}>
+      <span ref={lowerSelectRef}>
+        <Select
+          values={R.pipe(
+            R.range(MIN_YEAR, parseInt(yearRange[0])),
+            R.map(String),
+            R.reverse()
+          )}
+          selectedValue={String(parseInt(yearRange[0]) - 1)}
+          handleChange={handleYearChange}
+        />
+      </span>
+      {R.pipe(
+        R.range(parseInt(yearRange[0], 10), currentYearInt),
+        R.map((year) => (
+          <LinkAnchor key={year} href={`/${year}`} className="mx-1" important>
+            {year}
+          </LinkAnchor>
+        ))
+      )}
       <span ref={itemRef}>
         <LinkAnchor
-          href={prevYear ? `/${prevYear}` : undefined}
-          className="inline mx-1"
+          key={currentYearInt}
+          href={`/${currentYear}`}
+          className="mx-1"
+          active={Boolean(activeYear)}
           important
-          active={false}
         >
-          prev
+          {currentYear}
         </LinkAnchor>
       </span>
-      {R.map(years, (year) => (
-        <LinkAnchor
-          key={year}
-          href={`/${year}`}
-          active={year === activeYear}
-          className="mx-1"
-          important
-        >
-          {year}
-        </LinkAnchor>
-      ))}
-      <LinkAnchor
-        href={nextYear ? `/${nextYear}` : undefined}
-        className="mx-1"
-        important
-        active={false}
-      >
-        next
-      </LinkAnchor>
+      {R.pipe(
+        R.range(currentYearInt + 1, parseInt(yearRange[1], 10) + 1),
+        R.map((year) => (
+          <LinkAnchor key={year} href={`/${year}`} className="mx-1" important>
+            {year}
+          </LinkAnchor>
+        ))
+      )}
+      <span ref={upperSelectRef}>
+        <Select
+          values={R.pipe(
+            R.range(parseInt(yearRange[1]) + 1, MAX_YEAR + 1),
+            R.map(String)
+          )}
+          selectedValue={String(parseInt(yearRange[1]) + 1)}
+          handleChange={handleYearChange}
+        />
+      </span>
     </nav>
   );
 }
@@ -127,24 +186,22 @@ function RangeNav({ activeYear }: { activeYear: string }) {
     navigate(`/${lowerYear > value ? value : lowerYear}-${value}`);
   };
 
+  const values = R.range(MIN_YEAR, MAX_YEAR).map(String);
+
   return (
     <div>
       (
-      <select onChange={handleLowerYearChange} className="mx-1 outline-none">
-        {R.range(MIN_YEAR, MAX_YEAR + 1).map((value) => (
-          <option key={value} selected={String(value) === lowerYear}>
-            {value}
-          </option>
-        ))}
-      </select>
+      <Select
+        values={values}
+        selectedValue={lowerYear}
+        handleChange={handleLowerYearChange}
+      />
       -
-      <select onChange={handleUpperYearChange} className="mx-1 outline-none">
-        {R.range(MIN_YEAR, MAX_YEAR + 1).map((value) => (
-          <option key={value} selected={String(value) === upperYear}>
-            {value}
-          </option>
-        ))}
-      </select>
+      <Select
+        values={values}
+        selectedValue={upperYear}
+        handleChange={handleUpperYearChange}
+      />
       )
     </div>
   );
